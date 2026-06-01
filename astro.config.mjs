d88a18fca74ffb87231cfdc2e540c03e6fb3fcce
kkiwiio <kyaa0903@gmail.com>
2026-06-01 23:28:54 +0900

    switch to theme

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import icon from "astro-icon";
+import astroI18next from "astro-i18next";
+
+export default defineConfig({
+  site: "https://kkiwiio.github.io",
+  integrations: [vue(), icon(), astroI18next()],
+});
